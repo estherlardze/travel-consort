dc3c8e17dbe0224f54ac7855ec9df6e09b83997f
estherlardze <estherlardze8@gmail.com>
2023-08-27 09:45:50 +0000

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,18 @@ import { Header, Map, Places } from './components/index';
 
 const App = () => {
   return (
-    // <div className='w-full'>
-    //   <Header />
-    //   <div className='grid grid-cols-1 md:grid-cols-2'>
-    //     <div className=''>
-    //      <Places />
-    //     </div>
-    //     <div>
-    //       <Map />
-    //    </div>
-    //  </div>
+    <div className='w-full'>
+      <Header />
+      <div className='grid grid-cols-1 md:grid-cols-2 ps-4'>
+        <div className=''>
+         <Places />
+        </div>
+        <div>
+          <Map />
+       </div>
+     </div>
 
-    // </div>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    </div> 
    
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {AiOutlineSearch} from 'react-icons/ai'
 
 
 const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const handlesearch = (e) => {
+    setSearchTerm(e.target.value)
+  }
+  
   return (
-    <div>
-      nav
-      <button className='text-gray-500 bg-zinc-900'>Hey</button>
+    <div className='flex justify-between items-center bg-[#07ab4b] p-4 shadow-lg'>
+      <h3 className='font-bold text-lg cursor-pointer'>Travel Consort</h3>
+
+       <div className='flex justify-between'>
+         <h5 className='font-[600] me-3 hidden sm:flex'>Explore new places</h5>
+         
+         <div className='flex bg-[#e1f7ea] ps-1 rounded'>
+           <AiOutlineSearch color='#646262' className='mt-2 me-1'/>
+           <input type="text" placeholder='Search...' value={searchTerm} onChange={handlesearch} 
+             className='border-0 ps-2 p-1 outline-none rounded bg-[#e1f7ea]'/>
+        </div>      
+       </div>
     </div>
 
   )
